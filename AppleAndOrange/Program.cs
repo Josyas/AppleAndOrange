af44daa65eea904ea2669e5fd58b83a0e469e726
josia <josia@DESKTOP-19NMMMI>
2022-11-06 15:17:17 -0300

    Adicionar arquivos de projeto.

---
+++ AppleAndOrange/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AppleAndOrange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int s = 7, a = 4;
+            int t = 10, b = 12;
+
+            int[] apples = { 2, 3, -4 };
+            int[] oranges = { 3, -2, -4 };
+
+            int resultMaca = 0;
+            int resultLaranja = 0;
+
+            for (int i = 0; i < apples.Length; i++)
+            {
+                var appleStart = a + apples[i];
+                var appleFinish = a + apples[i];
+                var HomeStart = s; //7
+                var HomeFinish = t;//14
+
+                if (appleStart >= HomeStart && appleFinish <= HomeFinish)
+                {
+                    resultMaca++;
+                }
+            }
+
+            for (int i = 0; i < oranges.Length; i++)
+            {
+                var orangeStart = b + oranges[i];
+                var orangeFinish = b + oranges[i];
+                var HomeStart = s; //7
+                var HomeFinish = t; //14
+
+                if (orangeStart <= HomeFinish && orangeFinish >= HomeStart)
+                {
+                    resultLaranja++;
+                }
+            }
+
+            Console.WriteLine(resultMaca + " => maçã");
+            Console.WriteLine(resultLaranja + " => Laranja");
+        }
+    }
+}
